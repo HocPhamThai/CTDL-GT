f8840bd53ac952c83dcfc3450b6ebb0d57b5fccf
hocpham <phamthaihoc008@gmail.com>
2025-04-21 22:37:15 +0700

    Add project files.

---
+++ CTDL&&GT/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CTDL__GT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ MaxSum/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace MaxSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = {-2, 11, -4, 13, -5, -2 };
+            Console.WriteLine(MaxSumNested(a));
+        }
+
+        public static int MaxSumNested(int[] a)
+        {
+            int maxSum = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                for (int j = i; j < a.Length; j++)
+                {
+                    int sum = 0;
+                    for(int k = i; k < j; k++)
+                    {
+                        sum += a[k];
+                    }
+                    if (sum > maxSum)
+                    {
+                        maxSum = sum;
+                    }
+                }
+            }
+            return maxSum;
+        }
+    }
+}
