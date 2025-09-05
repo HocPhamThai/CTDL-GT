cc7f02c3f59def28a579be618f6e73f47fe0cd5e
hocpham <phamthaihoc008@gmail.com>
2025-09-05 22:58:49 +0700

    HashTable - Declaration for Hastable ADT, Node

---
+++ HashTable/HashTableADT.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable
+{
+    public interface HashTableADT<K, V> : IEnumerable<K>
+    {
+        int Size();
+        bool IsEmpty();
+        // hash code
+        int HashCodeToIndex(K key);
+        bool Has(K key);
+        V Insert(K key, V value);
+        V Get(K key);
+        V Remove(K key);
+    }
+}
---
+++ HashTable/Node.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable
+{
+    public class Node<K,V>
+    {
+        private int hash;
+        private K key;
+        private V value;
+
+
+        public int Hash { get => this.hash; set => this.hash = value; }
+        public K Key { get => this.key; set => this.key = value; }
+        public V Value { get => this.value; set => this.value = value; }
+
+
+        public Node(int hash, K key, V value)
+        {
+            this.key = key;
+            this.hash = key.GetHashCode();
+            this.value = value;
+        }
+
+        public bool Equals(Node<K,V> other)
+        {
+            if (other.hash != this.hash) return false;
+            return this.Key.Equals(other.key);
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
